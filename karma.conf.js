f88f0c4fb1edab035c2d30b6fa863d625cc5c467
Kamil Wasilonek <kamilwasilonek1@gmail.com>
2019-08-09 15:37:54 +0200

    Add test config file

--- karma.conf.js
+++ karma.conf.js
@@ -24,9 +24,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true, // Change it to "true" if you want to keep browser open after test finished
     restartOnFileChange: true
   });
 };
