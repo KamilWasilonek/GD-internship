cc5522326fedca8b7576a36a96a3e8b31587ac2c
Kamil Wasilonek <kamilwasilonek1@gmail.com>
2019-08-20 10:58:54 +0200

    Change social-bar icons

--- src/app/core/components/header/contact-bar/socials/socials.component.ts
+++ src/app/core/components/header/contact-bar/socials/socials.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SocialsService } from 'src/app/core/services/socials.service';
+import { SocialsService } from '@core/services/socials.service';
 
 @Component({
   selector: 'app-socials',
