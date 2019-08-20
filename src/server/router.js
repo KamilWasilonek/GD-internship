29193bf9f9a9021a7300d8032bcdb4737781a64d
rj <rj@gw-griddynamics.retn.net>
2019-08-20 12:00:54 +0200

    Fix routes paths

--- src/server/router.js
+++ src/server/router.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const controller = require('./controller');
 const memcacheMiddleware = require('./memcache.middleware');
 
-router.get('/products', memcacheMiddleware(), controller.getProducts);
-router.get('/products/:id',memcacheMiddleware(), controller.getProductById);
+router.get('/api/products', memcacheMiddleware(), controller.getProducts);
+router.get('/api/products/:id',memcacheMiddleware(), controller.getProductById);
 router.get('/api/filters', memcacheMiddleware(),controller.getFilters);
+router.get('**', controller.notFound);
 module.exports = router;
