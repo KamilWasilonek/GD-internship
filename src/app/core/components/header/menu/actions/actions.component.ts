05aa60b39611bc9502a9559db765d9b7d0a9d847
justyna <jusdan92@gmail.com>
2019-08-26 14:33:54 +0200

    Add menu icons

--- src/app/core/components/header/menu/actions/actions.component.ts
+++ src/app/core/components/header/menu/actions/actions.component.ts
@@ -1,12 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
+import { actionSvg } from '../../../../../../assets/config/actions-svg.js';
 
 @Component({
   selector: 'app-actions',
   templateUrl: './actions.component.html',
   styleUrls: ['./actions.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class ActionsComponent implements OnInit {
-  constructor() {}
+  @ViewChild('search', { static: true }) search: ElementRef;
+  @ViewChild('user', { static: true }) user: ElementRef;
+  @ViewChild('card', { static: true }) card: ElementRef;
 
-  ngOnInit() {}
+  constructor() {}
+  ngOnInit() {
+    for (const key in actionSvg) {
+      if (actionSvg.hasOwnProperty(key)) {
+        this[key].nativeElement.innerHTML = actionSvg[key].svg;
+      }
+    }
+  }
+
+  searchOnClick() {
+    console.log('search icon active');
+  }
+
+  userOnClick() {
+    console.log('user icon active');
+  }
+
+  cardOnClick() {
+    console.log('card icon active');
+  }
 }
