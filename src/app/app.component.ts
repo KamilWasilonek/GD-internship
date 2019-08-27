a741d8b7f128627f26a053f553ce3c9b474c0583
rj <rj@gw-griddynamics.retn.net>
2019-08-27 14:27:00 +0200

    Add different environments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GD-internship-angularProject';
+  environmentName = '';
+
+  constructor() {
+    this.environmentName = environment.environmentName;
+  }
+
+  private onDetectEnvironment() {
+    console.log(this.environmentName);
+  }
+  ngOnInit() {
+    this.onDetectEnvironment();
+  }
 }
