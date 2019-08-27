099bb6c71fc9e988ba67a2aedfbd072a71292b47
Kamil Wasilonek <kamilwasilonek1@gmail.com>
2019-08-27 16:29:43 +0200

    Add home-page components structure

---
+++ src/app/home-page/components/arrivals/arrivals.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arrivals',
+  templateUrl: './arrivals.component.html',
+  styleUrls: ['./arrivals.component.scss'],
+})
+export class ArrivalsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './components/home-page.component';
 import { HomePageRoutingModule } from './home-page-routing.module';
+import { BannerComponent } from './components/banner/banner.component';
+import { ArrivalsComponent } from './components/arrivals/arrivals.component';
+import { AdvertisementsComponent } from './components/advertisements/advertisements.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, BannerComponent, ArrivalsComponent, AdvertisementsComponent],
   imports: [CommonModule, HomePageRoutingModule],
 })
 export class HomePageModule {}
