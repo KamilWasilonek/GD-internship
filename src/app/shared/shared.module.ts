ef34f8076f0aace54f8bd1dead10b82d000abcb2
justyna <jusdan92@gmail.com>
2019-08-29 13:59:38 +0200

    Add basic product-item

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './components/spinner/spinner.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [SpinnerComponent],
-  imports: [CommonModule],
-  exports: [SpinnerComponent],
+  declarations: [SpinnerComponent, ProductItemComponent],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [ProductItemComponent, SpinnerComponent],
 })
 export class SharedModule {}
