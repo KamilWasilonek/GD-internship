7d548cb537591c7918a3efc4a3405cfd87817e49
rj <rj@gw-griddynamics.retn.net>
2019-09-02 10:37:21 +0200

    Fix view more component

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -7,10 +7,11 @@ import { HomePageRoutingModule } from './home-page-routing.module';
 import { BannerComponent } from './components/banner/banner.component';
 import { ArrivalsComponent } from './components/arrivals/arrivals.component';
 import { AdvertisementsComponent } from './components/advertisements/advertisements.component';
+import { ViewMoreComponent } from './components/view-more/view-more.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [HomePageComponent, BannerComponent, ArrivalsComponent, AdvertisementsComponent],
+  declarations: [HomePageComponent, BannerComponent, ArrivalsComponent, AdvertisementsComponent,ViewMoreComponent],
   imports: [CommonModule, HomePageRoutingModule, HttpClientModule, SharedModule],
 })
 export class HomePageModule {}
