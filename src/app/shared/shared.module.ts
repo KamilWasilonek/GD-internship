823a9a087e2965863669cee6cc68e1b7fea1fc2d
rj <rj@cpe-08-02-rs.krapod.hor.net.pl>
2019-09-03 16:40:42 +0200

    Finish desktop styling
    Change shared.module.ts to add router functionality to logo component and finish desktop styles for footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { SmallTitleComponent } from './components/small-title/small-title.component';
@@ -8,7 +9,7 @@ import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [ProductItemComponent, SmallTitleComponent, SpinnerComponent, LogoComponent],
-  imports: [CommonModule, FontAwesomeModule],
-  exports: [ProductItemComponent, SmallTitleComponent, SpinnerComponent, LogoComponent],
+  imports: [CommonModule, FontAwesomeModule, RouterModule],
+  exports: [ProductItemComponent, SmallTitleComponent, SpinnerComponent, LogoComponent, RouterModule],
 })
 export class SharedModule {}
