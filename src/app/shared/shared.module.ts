eff75fe33db299ad932088fa4f1721ff36055f8c
rj <rj@cpe-08-02-rs.krapod.hor.net.pl>
2019-09-03 14:35:20 +0200

    Add Footer structure
    Add icons for footer, create logo component, basic styling

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { SpinnerComponent } from './components/spinner/spinner.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { SmallTitleComponent } from './components/small-title/small-title.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
-  declarations: [ProductItemComponent, SmallTitleComponent, SpinnerComponent],
+  declarations: [ProductItemComponent, SmallTitleComponent, SpinnerComponent, LogoComponent],
   imports: [CommonModule, FontAwesomeModule],
-  exports: [ProductItemComponent, SmallTitleComponent, SpinnerComponent],
+  exports: [ProductItemComponent, SmallTitleComponent, SpinnerComponent, LogoComponent],
 })
 export class SharedModule {}
