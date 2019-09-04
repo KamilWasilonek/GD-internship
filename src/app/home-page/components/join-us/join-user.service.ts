0a11b8582bb422a860870176a2648e1d2bfaa3e4
dudzon <tomasz.j.p.dudzinski@gmail.com>
2019-09-05 00:09:51 +0200

    Add focus on email input in Join Us
    When user clicks join us link in footer, email in Join us gets focus

--- src/app/home-page/components/join-us/join-user.service.ts
+++ src/app/home-page/components/join-us/join-user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UserSubscription } from './user-subscription';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +10,18 @@ export class JoinUserService {
   // apiURL = 'https://project-angular-gd.herokuapp.com/api';
   apiURL = 'http://localhost:3000/api';
 
+  public linkWasClicked = false;
+  public linkIsClicked = new Subject<boolean>();
+
   constructor(private http: HttpClient) {}
 
   public createSubscription(userSubscription: UserSubscription) {
     return this.http.post(`${this.apiURL}/subscriptions`, userSubscription);
+  }
+  sendClick() {
+    return this.linkIsClicked.next((this.linkWasClicked = true));
+  }
+  receiveClick() {
+    return this.linkWasClicked;
   }
 }
