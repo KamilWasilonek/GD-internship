c937a761922592026d91354f35d3c992ce4b6e50
Kamil Wasilonek <kamilwasilonek1@gmail.com>
2019-09-05 15:14:02 +0200

    Fix banner images loading

--- src/app/home-page/components/banner/banner.component.ts
+++ src/app/home-page/components/banner/banner.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnDestroy } from '@angular/core';
 import { ISlide } from '@app/shared/interfaces/banner.interface';
+import { Component, OnDestroy, SecurityContext } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
 import { DomSanitizer } from '@angular/platform-browser';
-import { delay } from 'rxjs/operators';
 import { SliderService } from '@app/shared/services/slider.service';
 
 @Component({
@@ -15,72 +14,85 @@ export class BannerComponent implements OnDestroy {
   slides: ISlide[];
   currentSlideIndex: number;
   backgroundImage: string;
+  sliderHttpObserver: Subscription;
   sliderInterval: Subscription;
   spinner = {
     message: 'Loading latest articles',
     isError: false,
   };
 
+  title: string;
+  proposition: string;
+  description: string;
+  price: string;
+
   constructor(private sliderService: SliderService, private sanitizer: DomSanitizer) {
-    this.sliderService
-      .getSlideshow()
-      .pipe(delay(3000))
-      .subscribe(
-        slideshow => {
-          this.slides = slideshow;
-        },
-        error => {
-          this.spinner = {
-            message: 'Can not load latest products',
-            isError: true,
-          };
-        },
-        () => {
-          this.currentSlideIndex = 0;
-          this.backgroundImage = this.slides[this.currentSlideIndex].img;
-          this.isDataLoading = false;
-          this.startSliderInterval();
-        }
-      );
+    this.sliderHttpObserver = this.sliderService.getSlideshow().subscribe(
+      slideshow => {
+        this.slides = slideshow;
+      },
+      error => {
+        this.spinner = {
+          message: 'Can not load latest products',
+          isError: true,
+        };
+      },
+      () => {
+        this.currentSlideIndex = 0;
+        this.backgroundImage = this.sanitizeImage(this.slides[this.currentSlideIndex].img);
+        this.startSliderInterval();
+      }
+    );
   }
 
   ngOnDestroy() {
     if (this.sliderInterval !== undefined) {
       this.sliderInterval.unsubscribe();
     }
+    if (this.sliderHttpObserver !== undefined) {
+      this.sliderHttpObserver.unsubscribe();
+    }
   }
 
-  public showNextSlide(event?: MouseEvent) {
+  public showNextSlide(event?: MouseEvent): void {
     if (event !== undefined && this.sliderInterval !== undefined) {
       this.sliderInterval.unsubscribe();
     }
     if (this.currentSlideIndex++ >= this.slides.length - 1) {
       this.currentSlideIndex = 0;
     }
-    this.backgroundImage = this.slides[this.currentSlideIndex].img;
+    this.backgroundImage = this.sanitizeImage(this.slides[this.currentSlideIndex].img);
   }
 
-  public showPrevSlide(event?: MouseEvent) {
+  public showPrevSlide(event?: MouseEvent): void {
     if (event !== undefined && this.sliderInterval !== undefined) {
       this.sliderInterval.unsubscribe();
     }
     if (this.currentSlideIndex-- === 0) {
       this.currentSlideIndex = this.slides.length - 1;
     }
-    this.backgroundImage = this.slides[this.currentSlideIndex].img;
+    this.backgroundImage = this.sanitizeImage(this.slides[this.currentSlideIndex].img);
   }
 
-  startSliderInterval() {
+  startSliderInterval(): void {
     this.sliderInterval = interval(5000).subscribe(() => {
       this.showNextSlide();
     });
   }
 
-  public makeOrder() {
+  public makeOrder(): void {
     console.log('Ordered');
   }
 
-  public sanitizeImage(image: string) {
-    return this.sanitizer.bypassSecurityTrustStyle(`url(${image})`);
+  public setSlideContent(index: number): void {
+    this.title = this.slides[index].title;
+    this.proposition = this.slides[index].proposition;
+    this.description = this.slides[index].description;
+    this.price = this.slides[index].price;
+    this.isDataLoading = false;
+  }
+
+  public sanitizeImage(url: string): string {
+    return this.sanitizer.sanitize(SecurityContext.URL, url);
   }
 }
