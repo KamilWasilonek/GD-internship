2097f79e7284a06441dec1d3d5c0a001c98c6fe4
Kamil Wasilonek <kamilwasilonek1@gmail.com>
2019-09-06 16:17:54 +0200

    Change api routes

--- src/app/shared/api-endpoints.ts
+++ src/app/shared/api-endpoints.ts
@@ -1,7 +1,7 @@
-const slideshowURL = 'http://localhost:3000/api/slideshow';
-const advertismentsURL = 'http://localhost:3000/api/advertisments';
-const subscriptionsURL = 'http://localhost:3000/api/subscriptions';
-const socialsURL = 'http://localhost:3000/api/socials';
+const slideshowURL = 'https://project-angular-gd.herokuapp.com/api/slideshow';
+const advertismentsURL = 'https://project-angular-gd.herokuapp.com/api/advertisments';
+const subscriptionsURL = 'https://project-angular-gd.herokuapp.com/api/subscriptions';
+const socialsURL = 'https://project-angular-gd.herokuapp.com/api/socials';
 const bestsellersURL = 'https://project-angular-gd.herokuapp.com/api/products?ids=1,3,6';
 
 export { slideshowURL, advertismentsURL, subscriptionsURL, socialsURL, bestsellersURL };
