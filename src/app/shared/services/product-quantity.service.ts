3e9e55b969e45d1357f467045806c0a97a07a18e
justyna <jusdan92@gmail.com>
2019-09-13 11:47:33 +0200

    Update product-quantity service

--- src/app/shared/services/product-quantity.service.ts
+++ src/app/shared/services/product-quantity.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
+import { IOrder } from '../interfaces/product-detail/ordered-product.interface';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductQuantity {
-  orderProductQuantity = new Subject<number>();
+export class ProductQuantityService {
+  private orderDetails = {
+    size: 'S',
+    quantity: 1,
+  };
 
-  changeProductQuantity(amount: number) {
-    this.orderProductQuantity.next(amount);
+  currentOrderDetails = new Subject<IOrder>();
+
+  getOrderDetails(): Observable<IOrder> {
+    return this.currentOrderDetails.asObservable();
+  }
+
+  changeOrderSize(size: string): void {
+    this.orderDetails.size = size;
+    this.currentOrderDetails.next(this.orderDetails);
+  }
+
+  changeOrderQuantity(quantity: number): void {
+    this.orderDetails.quantity = quantity;
+    this.currentOrderDetails.next(this.orderDetails);
   }
 }
